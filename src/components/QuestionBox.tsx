0b4e818a5645340b930c61352f80387909f8b40b
Surya Vasudev <suryavasudev05@gmail.com>
2025-10-04 16:33:51 -0400

    fix errors

--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,5 +1,9 @@
 import { Box, Text } from "@chakra-ui/react";
-const QuestionBox = ({ question }) => {
+interface QuestionBoxProps {
+    question: string;
+}
+
+const QuestionBox = ({ question }: QuestionBoxProps) => {
     return (
         <Box p={4} borderWidth={1} borderRadius="md" bg="blackAlpha.300" borderColor="pink.100" mx={10} my={4}>
             <Text fontWeight="bold">Question:</Text>
