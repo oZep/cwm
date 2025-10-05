a3c59c3d0600d679bc7a869085ec537a7cfadd98
Joey Issa <joeyissa59@gmail.com>
2025-10-05 07:58:12 -0400

    fx. question formatting

--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,4 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
+import { marked } from "marked";
+
 interface QuestionBoxProps {
     question: string;
 }
@@ -7,9 +9,8 @@ const QuestionBox = ({ question }: QuestionBoxProps) => {
     return (
         <div>
         <Box p={4} borderWidth={1} borderRadius="md" bg="blackAlpha.300" borderColor="pink.100" mx={10} my={4}>
-            <Text fontWeight="bold">Question:</Text>
             <Text>
-                {question}
+                <span dangerouslySetInnerHTML={{ __html: marked(question) }} />
             </Text>
         </Box>
         </div>
